596627408b0381a74cec7b4a4c312ded6e3e811f
Mano-jk <32917899+Mano-jk@users.noreply.github.com>
2020-09-20 20:40:13 +0530

    Update karma.conf.js
    Updated for JUnit

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -4,28 +4,26 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('karma-junit-reporter'),
+      require('@angular/cli/plugins/karma')
     ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+ reporters: ['junit'],    
+    junitReporter: { 
+ outputDir: 'karma-results',    
+ outputFile: 'karma-results.xml'    
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+    angularCli: {
+      environment: 'dev',
     },
-    reporters: ['progress', 'kjhtml'],
     port: 9876,
-    colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+ browsers: ['ChromeHeadless'],    
+ autoWatch: false,    
+ singleRun: true    
   });
 };
