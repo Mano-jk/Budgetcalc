9305dac7633647912e2ddab871ecd096f3e3b789
Mano-jk <32917899+Mano-jk@users.noreply.github.com>
2020-09-26 16:41:47 +0530

    Create Test.py

---
+++ Test/Test.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+#driver.implicitly_wait(10)
+from selenium.webdriver.chrome.options import Options
+
+option=Options()
+option.add_argument("--headless")   
+option.add_argument('--no-sandbox')
+option.add_argument('start-maximized')
+option.add_argument('disable-infobars')
+option.add_argument('--disable-extensions')
+option.add_argument('--disable-dev-shm-usage')
+
+def test_login():
+    global driver
+    driver=webdriver.Chrome("./testing/83/chromedriver", options=option)
+    driver.implicitly_wait(10)
+    driver.get('http://localhost:80')
+    print(driver.title)
+def test_title_check():
+    assert (driver.title == 'Budget Calculator'), 'title not matched'
+    driver.close()
+    
