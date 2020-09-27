d63925e65156999c63419307b16785c8f5e9f651
Mano-jk <32917899+Mano-jk@users.noreply.github.com>
2020-09-27 21:18:28 +0530

    Update Test.py

--- Test/Test.py
+++ Test/Test.py
@@ -39,25 +39,4 @@ def test_budget(self):
         driver.find_element_by_name("description").send_keys("Rent")
         driver.find_element_by_xpath("//button/p").click()
     
-    def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-    
 driver.close()
